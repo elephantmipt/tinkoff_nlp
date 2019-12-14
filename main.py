267c83108978ac2263d81d87511d637e70657d63
Nikita <Balaganskij.NN@phystech.edu>
2019-12-14 20:38:22 +0300

    mhsa and preprocessing

--- main.py
+++ main.py
@@ -1,53 +1,119 @@
-
-import math
-
 import torch
-import torch.nn as nn
-
+from itertools import chain
+from typing import Dict, Iterable, Union, Optional, List
 import torch.optim as optim
-import numpy as np
-import re
-
+import math
 from tqdm import tqdm
 
-from allennlp.data import Instance
-from allennlp.data.fields import TextField, SequenceLabelField
-from allennlp.data.dataset_readers import DatasetReader
 
-from allennlp.data import Instance
-from allennlp.data.fields import TextField, SequenceLabelField
 from language_model import LanguageModel
-from allennlp.data.dataset_readers import DatasetReader, LanguageModelingReader, SimpleLanguageModelingDatasetReader
 from allennlp.common.file_utils import cached_path
-from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Token
 from allennlp.data.vocabulary import Vocabulary
-from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
+from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import Embedding
-from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder, PytorchSeq2SeqWrapper
-from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
-from allennlp.data.iterators import BucketIterator, DataIterator
+from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper
+from allennlp.modules.seq2seq_encoders.multi_head_self_attention import MultiHeadSelfAttention
+from allennlp.data.iterators import BucketIterator
 from allennlp.training.trainer import Trainer
-from preprocessing import Preprocessing
-from allennlp.predictors import SentenceTaggerPredictor
+from preprocessing import Preprocessing, _preproc
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import TextField
+from allennlp.data.instance import Instance
+from allennlp.data.token_indexers import SingleIdTokenIndexer
+from allennlp.data.token_indexers.token_indexer import TokenIndexer
+from allennlp.data.tokenizers import WordTokenizer
+from allennlp.data.tokenizers.tokenizer import Tokenizer
 
-from metrics import Perplexity
 
 torch.manual_seed(1)
 
 PATH = '../cache/'
 
-preprocessing = Preprocessing(inp_path=PATH+'data.csv', out_path=PATH+'prep_data.csv')
+BPE = False
 
-reader = SimpleLanguageModelingDatasetReader()
+preprocessing = Preprocessing(inp_path=PATH+'data.csv', train_path=PATH+'train_data.csv',
+                              test_path=PATH+'test_data.csv', bpe=BPE, model_path=PATH+'bpe_yttm.model')
 
-train_dataset = reader.read(cached_path(PATH + 'prep_data.csv'))
+
+class LanguageModelReader(DatasetReader):
+
+    def __init__(self,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 max_sequence_length: int = None) -> None:
+        super().__init__(lazy=False)
+        self._tokenizer = tokenizer or WordTokenizer()
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+        if max_sequence_length is not None:
+            self._max_sequence_length: Union[float, Optional[int]] = max_sequence_length
+        else:
+            self._max_sequence_length = math.inf
+
+    def text_to_instance(self,  # type: ignore
+                         sentence: str) -> Instance:
+        # pylint: disable=arguments-differ
+        tokenized = self._tokenizer.tokenize(sentence)
+        return_instance = Instance({
+                'source': TextField(tokenized, self._token_indexers),
+        })
+        return return_instance
+
+    def _read(self, file_path: str) -> Iterable[Instance]:
+        with open(file_path) as file:
+            pbar = tqdm(file)
+            pbar.set_description('applying regular expressions')
+            for sentence in pbar:
+                sentence = _preproc(sentence)
+                instance = self.text_to_instance(sentence)
+                if instance.fields['source'].sequence_length() <= self._max_sequence_length:
+                    yield instance
+
+class LanguageModelBpeReader(DatasetReader):
+
+    def __init__(self,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 max_sequence_length: int = None) -> None:
+        super().__init__(lazy=False)
+        self._tokenizer = tokenizer or WordTokenizer()
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+        if max_sequence_length is not None:
+            self._max_sequence_length: Union[float, Optional[int]] = max_sequence_length
+        else:
+            self._max_sequence_length = math.inf
+
+    def text_to_instance(self,  # type: ignore
+                         sentence: str) -> Instance:
+        # pylint: disable=arguments-differ
+        tokenized = self._tokenizer.tokenize(sentence)
+        return_instance = Instance({
+                'source': TextField(tokenized, self._token_indexers),
+        })
+        return return_instance
+
+    def _read(self, file_path: str) -> Iterable[Instance]:
+        with open(file_path) as file:
+            pbar = tqdm(file)
+            pbar.set_description('applying regular expressions')
+            for sentence in pbar:
+                sentence = _preproc(sentence)
+                instance = self.text_to_instance(sentence)
+                if instance.fields['source'].sequence_length() <= self._max_sequence_length:
+                    yield instance
 
 
-EMBEDDING_DIM = 6
-HIDDEN_DIM = 6
+reader = LanguageModelReader(max_sequence_length=30)
 
-vocab = Vocabulary.from_instances(train_dataset)
+
+train_dataset = reader.read(cached_path(PATH + 'train_data.csv'))
+test_dataset = reader.read(cached_path(PATH + 'test_data.csv'))
+
+
+EMBEDDING_DIM = 32
+HIDDEN_DIM = 32
+
+
+vocab = Vocabulary.from_instances(chain(train_dataset,test_dataset))
 
 token_embedding = Embedding(num_embeddings=vocab.get_vocab_size('tokens'),
                             embedding_dim=EMBEDDING_DIM)
@@ -56,19 +122,30 @@ word_embeddings = BasicTextFieldEmbedder({"tokens": token_embedding})
 
 lstm = PytorchSeq2SeqWrapper(torch.nn.LSTM(EMBEDDING_DIM, HIDDEN_DIM, batch_first=True))
 
+transformer = MultiHeadSelfAttention(num_heads=8, input_dim=EMBEDDING_DIM,
+                                     attention_dim=16, values_dim=16)
+
+transformer_model = LanguageModel(contextualizer=transformer,
+                                  text_field_embedder=word_embeddings,
+                           vocab=vocab)
+
 lstm_model = LanguageModel(contextualizer=lstm, text_field_embedder=word_embeddings,
                            vocab=vocab)
 print(vocab.get_vocab_size('tokens'))
 
-optimizer = optim.SGD(lstm_model.parameters(), lr=0.1)
-iterator = BucketIterator(batch_size=2, sorting_keys=[("source", "num_tokens")])
+  #elmo=Elmo(num_output_representations=512, )
+
+optimizer = optim.Adam(transformer_model.parameters())
+iterator = BucketIterator(batch_size=16, sorting_keys=[("source", "num_tokens")])
 iterator.index_with(vocab)
-trainer = Trainer(model=lstm_model,
+trainer_transformer = Trainer(model=transformer_model,
                   optimizer=optimizer,
                   iterator=iterator,
                   train_dataset=train_dataset,
-                  #validation_dataset=validation_dataset,
-                  patience=10,
-                  num_epochs=1000)
+                  validation_dataset=test_dataset,
+                  patience=3,
+                  num_epochs=100,
+                  serialization_dir='./tb/transformer',
+                  )
 
-trainer.train()
+trainer_transformer.train()
--- preprocessing.py
+++ preprocessing.py
@@ -1,49 +1,62 @@
 import re
 from tqdm import tqdm
 from typing import List
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import youtokentome as yttm
+
+
+
 
 class Preprocessing():
-    def __init__(self, inp_path, out_path):
-        with open(inp_path, 'r') as inp:
-            with open(out_path, 'w') as out:
-                pbar = tqdm(inp)
-                pbar.set_description('Preprocessing')
-                for line in pbar:
-                    if len(self._preproc(line).split()) != 0:
-                        out.write(self._preproc(line) + '\n')
+    def __init__(self, inp_path, train_path, test_path, bpe=False, model_path='bpe_yttm.model', vocab_size=5000):
+        df = pd.read_csv(inp_path)
+        if bpe:
+            print('training bpe...')
+            yttm.BPE.train(data=" ".join(df.msg.values), model=model_path, vocab_size=vocab_size)
+            print('done')
+        X_train, X_test = train_test_split(df.msg, random_state=1, test_size=0.1)
+        with open(train_path, 'w') as out:
+            for msg in X_train:
+                out.write(msg+'\n')
+        with open(test_path, 'w') as out:
+            for msg in X_test:
+                out.write(msg+'\n')
 
-    def _preproc(self, msg: str) -> List[str]:
-        x = msg
-        number_re = r'[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*'
-        x = re.sub('[\d]{4,99}', ' [phone_number] ', x)
-        x = re.sub(number_re, ' [number] ', x)
-        x = x.strip().lower()
-        x = re.sub('[\s.]л[\s.]', ' лет ', x)
-        x = re.sub('[\s.]г[\s.]', ' года ', x)
-        x = re.sub('[\s.]м[\s.]', ' мужчина ', x)
-        x = re.sub('^м[\s.]', ' мужчина ', x)
-        x = re.sub('[\s.]ж[\s.]', ' женщина ', x)
-        x = re.sub('[\s.]женщ[\s.]', ' женщина ', x)
-        x = re.sub('^ж[\s.]', ' женщина ', x)
-        x = re.sub('[\s.]д[\s.]', ' девушка ', x)
-        x = re.sub('[\s.]дев[\s.]', ' девушка ', x)
-        x = re.sub('[\s.]поз[\s.]', ' познакомится ', x)
-        x = re.sub('^поз[\s.]', ' познакомится ', x)
-        x = re.sub('[\s.]позн[\s.]', ' познакомится ', x)
-        x = re.sub('^позн[\s.]', ' познакомится ', x)
-        x = re.sub('^познк[\s.]', ' познакомится ', x)
-        x = re.sub('^д[\s.]', 'девушка ', x)
-        x = re.sub('[\s.]п[\s.]', ' парень ', x)
-        x = re.sub('^п[\s.0-9]', ' парень ', x)
-        x = re.sub('[\s]пар[\s.]', ' парень ', x)
-        x = re.sub('^пар[\s.]', ' парень ', x)
-        x = re.sub('[\s]жен[\s.]', ' женщина ', x)
-        x = re.sub('норм[\s.]', 'нормальный ', x)
-        x = re.sub('симп[\s.]', ' симпотичная ', x)
-        x = re.sub('сим[\s.]', ' симпотичным ', x)
-        x = re.sub('сер[\s.]', 'серьезных ', x)
-        x = re.sub('отн[\s.]', 'отношений ', x)
-        x = re.sub('[\s.]с\\о[\s.]', ' серьезных отношений ', x)
-        x = re.sub('[.?!,]', ' ', x)
-        x = x.strip().lower()
-        return x
+
+def _preproc(self, msg: str) -> str:
+    x = msg
+    number_re = r'[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*'
+    x = re.sub('[\d]{4,99}', ' [phone_number] ', x)
+    x = re.sub(number_re, ' [number] ', x)
+    x = x.strip().lower()
+    x = re.sub('[\s.]л[\s.]', ' лет ', x)
+    x = re.sub('[\s.]г[\s.]', ' года ', x)
+    x = re.sub('[\s.]м[\s.]', ' мужчина ', x)
+    x = re.sub('^м[\s.]', ' мужчина ', x)
+    x = re.sub('[\s.]ж[\s.]', ' женщина ', x)
+    x = re.sub('[\s.]женщ[\s.]', ' женщина ', x)
+    x = re.sub('^ж[\s.]', ' женщина ', x)
+    x = re.sub('[\s.]д[\s.]', ' девушка ', x)
+    x = re.sub('[\s.]дев[\s.]', ' девушка ', x)
+    x = re.sub('[\s.]поз[\s.]', ' познакомится ', x)
+    x = re.sub('^поз[\s.]', ' познакомится ', x)
+    x = re.sub('[\s.]позн[\s.]', ' познакомится ', x)
+    x = re.sub('^позн[\s.]', ' познакомится ', x)
+    x = re.sub('^познк[\s.]', ' познакомится ', x)
+    x = re.sub('^д[\s.]', 'девушка ', x)
+    x = re.sub('[\s.]п[\s.]', ' парень ', x)
+    x = re.sub('^п[\s.0-9]', ' парень ', x)
+    x = re.sub('[\s]пар[\s.]', ' парень ', x)
+    x = re.sub('^пар[\s.]', ' парень ', x)
+    x = re.sub('[\s]жен[\s.]', ' женщина ', x)
+    x = re.sub('норм[\s.]', 'нормальный ', x)
+    x = re.sub('симп[\s.]', ' симпотичная ', x)
+    x = re.sub('сим[\s.]', ' симпотичным ', x)
+    x = re.sub('сер[\s.]', 'серьезных ', x)
+    x = re.sub('отн[\s.]', 'отношений ', x)
+    x = re.sub('[\s.]с\\о[\s.]', ' серьезных отношений ', x)
+    x = re.sub('[.?!,]', ' ', x)
+    x = x.strip().lower()
+    return x
+
